88fbca1d0099c82ce887b83a943d48be12f0640d
John Servo <jaservo@up.edu.ph>
2022-06-26 22:03:29 +0800

    add X & O on click

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+
+
+const board = document.getElementById('board');
+const cells = board.querySelectorAll('.cell');
+
+// Displays and alternates between X & Y
+let flag = 0;
+cells.forEach(cell => {
+    
+    cell.addEventListener("click", e => {
+        if(flag === 0) {
+            cell.textContent = "X";
+            flag = 1;
+        } else {
+            cell.textContent = "O";
+            flag = 0;
+        }
+    });
+
+});
+
